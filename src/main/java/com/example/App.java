1f35336f0f514b89932604064ba65a5200d68a26
Yoganand30 <yoganandmaskalle@gmail.com>
2025-05-14 13:11:02 +0000

    Initial commit of maven projet

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int a=5;
+    	int b=2;
+    	int sum=a+b;
+        System.out.println( "sum is:"+sum);
+    }
+}
